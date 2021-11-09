c9d4b32c638af6b52e16133bdd7876375c84446a
HaiNguyen <nguyenhuuhai06102002@gmail.com>
2021-11-09 14:29:04 +0700

    baitapcode

---
+++ BTVN-C/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace BTVN_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Chương trình nhập xuất thông tin ứng viên");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("***Mời nhập ứng viên số 1");
+            Console.WriteLine("Họ và tên: ");
+            Console.WriteLine("Nhập năm sinh: ");
+            Console.WriteLine("Nhập giới tính: ");
+            Console.WriteLine("Nhập chiều cao: ");
+            Console.WriteLine("Nhập cân nặng: ");
+            Console.WriteLine("Nhập quê quán: ");
+            Console.WriteLine("Nhập chuyên ngành: ");
+            Console.WriteLine("Nhập điểm TB");
+            Console.WriteLine("***Mời nhập ứng viên số 2");
+            Console.WriteLine("Họ và tên: ");
+            Console.WriteLine("Nhập năm sinh: ");
+            Console.WriteLine("Nhập giới tính: ");
+            Console.WriteLine("Nhập chiều cao: ");
+            Console.WriteLine("Nhập cân nặng: ");
+            Console.WriteLine("Nhập quê quán: ");
+            Console.WriteLine("Nhập chuyên ngành: ");
+            Console.WriteLine("Nhập điểm TB");
+            Console.WriteLine("***Mời nhập ứng viên số 3");
+            Console.WriteLine("Họ và tên: ");
+            Console.WriteLine("Nhập năm sinh: ");
+            Console.WriteLine("Nhập giới tính: ");
+            Console.WriteLine("Nhập chiều cao: ");
+            Console.WriteLine("Nhập cân nặng: ");
+            Console.WriteLine("Nhập quê quán: ");
+            Console.WriteLine("Nhập chuyên ngành: ");
+            Console.WriteLine("Nhập điểm TB");
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Danh sách các ứng viên đã nhập");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("***Mời nhập ứng viên số 1");
+            Console.WriteLine("Họ và tên: Nguyễn Văn Tèo ");
+            Console.WriteLine("Nhập năm sinh: 1998");
+            Console.WriteLine("Nhập giới tính: F");
+            Console.WriteLine("Nhập chiều cao: 1.62");
+            Console.WriteLine("Nhập cân nặng: 52");
+            Console.WriteLine("Nhập quê quán: TP.HoChiMinh");
+            Console.WriteLine("Nhập chuyên ngành: Thiết Kế");
+            Console.WriteLine("Nhập điểm TB: 7.5");
+            Console.WriteLine("***Mời nhập ứng viên số 2");
+            Console.WriteLine("Họ và tên: Trần Thị Tèo ");
+            Console.WriteLine("Nhập năm sinh: 1996");
+            Console.WriteLine("Nhập giới tính: M");
+            Console.WriteLine("Nhập chiều cao: 1.72");
+            Console.WriteLine("Nhập cân nặng: 65");
+            Console.WriteLine("Nhập quê quán: Hà Nội");
+            Console.WriteLine("Nhập chuyên ngành: Game Dev");
+            Console.WriteLine("Nhập điểm TB: 8.1");
+            Console.WriteLine("***Mời nhập ứng viên số 3");
+            Console.WriteLine("Họ và tên: Lê Hoàng Tèo ");
+            Console.WriteLine("Nhập năm sinh: 1997 ");
+            Console.WriteLine("Nhập giới tính: M");
+            Console.WriteLine("Nhập chiều cao: 1.68");
+            Console.WriteLine("Nhập cân nặng: 57");
+            Console.WriteLine("Nhập quê quán: Bình Dương");
+            Console.WriteLine("Nhập chuyên ngành: Mobile Dev");
+            Console.WriteLine("Nhập điểm TB: 7.8");
+        }
+    }
+}
